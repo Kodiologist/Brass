89e0429fbbb53fc368e518003341393513d905d4
Kodi Arfer <git@arfer.net>
2013-05-31 14:01:06 -0400

    Made an instruction that the first subject ignored more prominnent.

--- commitments.py
+++ commitments.py
@@ -32,10 +32,10 @@ def get_activities():
     their commitments about."""
     dlg = ActivityListDialog(
         "Over the next fourteen days (two weeks), are there any activities that you plan to spend certain amounts of time on on certain days? For example, perhaps you want to exercise for an hour tomorrow, or study for half an hour every day. List any such activities below. On the next screen, we'll ask how much time you intend to spend on each activity each day. When choosing what to write here, however:\n\n"
+        u'• Leave fields blank rather than writing "N/A" or the like.\n\n'
         u"• Don't include activities if you don't have plans for particular days. For example, don't list exercise if you plan to exercise five days a week but you haven't yet decided which days.\n\n"
         u"• Don't include activities you're obliged to do and don't especially want to accomplish. For example, you might spend an hour every day commuting, but you probably aren't interested in commuting for its own sake.\n\n"
-        u"• Don't include sleep habits. We'll ask about those separately.\n\n"
-        u'• Leave fields blank rather than writing "N/A" or the like.\n\n')
+        u"• Don't include sleep habits. We'll ask about those separately.\n\n")
     dlg.CenterOnScreen(wx.BOTH)
     dlg.ShowModal()
     dlg.Destroy()
